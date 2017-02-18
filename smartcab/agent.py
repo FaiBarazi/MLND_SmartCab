b4823bdaff2e9719ba22d6d71c20a67346ba8afd
FaiBarazi <f.barazi@gmail.com>
2017-02-18 15:53:30 -0800

    Add optimized learning Log file, smartcab.ipynb and agent.py after successful implementation

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -23,7 +23,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
-        self.trial = 0
+        self.trial = 1
 
 
     def reset(self, destination=None, testing=False):
@@ -46,9 +46,11 @@ class LearningAgent(Agent):
 
         else:
             #Epsilon doesn't update on first trial
+            a = 0.9
+           
             
+            self.epsilon = 1.0/(self.trial**(0.5))
             self.trial+=1
-            self.epsilon = 1.0/(self.trial**2)
         return None
 
     def build_state(self):
@@ -174,7 +176,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent,learning = True,epsilon = 1.0, alpha = 0.5)
+    agent = env.create_agent(LearningAgent,learning = True,epsilon = 1.0, alpha = 0.8)
     
     ##############
     # Follow the driving agent
@@ -189,14 +191,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env,update_delay = 0.01,log_metrics = True,optimized = True,
+    sim = Simulator(env,update_delay = 0.01,log_metrics = False,optimized = True,
     display = False)
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test =10,tolerance = 0.01)
+    sim.run(n_test =10,tolerance = 0.082)
 
 
 if __name__ == '__main__':
